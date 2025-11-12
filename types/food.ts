14990f2f2aa1d0fcfeeaebe1cd74818505b35548
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2025-11-12 03:24:54 +0200

    Update food.ts

--- types/food.ts
+++ types/food.ts
@@ -1,9 +1,13 @@
 export interface Food {
   id: string;
-  food_name: string;
-  food_rating: string;
-  food_image: string;
-  restaurant_name: string;
-  restaurant_logo: string;
-  restaurant_status: "Open Now" | "Closed";
+  food_name?: string;
+  name?: string;
+  food_rating?: string;
+  rating?: string;
+  food_image?: string;
+  image?: string;
+  restaurant_name?: string;
+  restaurant_logo?: string;
+  restaurant_status?: string;
+  [key: string]: any;
 }
