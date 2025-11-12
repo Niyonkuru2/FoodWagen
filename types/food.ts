149781a914e0a310357dc5c68f3680ed2918b22d
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2025-11-12 03:36:57 +0200

    Update food.ts

--- types/food.ts
+++ types/food.ts
@@ -1,13 +1,11 @@
 export interface Food {
   id: string;
-  food_name?: string;
-  name?: string;
-  food_rating?: string;
-  rating?: string;
-  food_image?: string;
-  image?: string;
-  restaurant_name?: string;
-  restaurant_logo?: string;
-  restaurant_status?: string;
+  food_name: string;
+  food_rating: string;
+  food_image: string;
+  restaurant_name: string;
+  restaurant_logo: string;
+  restaurant_status: string;
+  price: string;
   [key: string]: any;
 }
